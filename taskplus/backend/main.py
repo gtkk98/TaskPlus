e7100238650f2d0617dcac2cec4bd241609de581
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-05-04 22:53:12 +0530

    Created table and successfully connect database.

--- taskplus/backend/main.py
+++ taskplus/backend/main.py
@@ -1,4 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+import model
+from database import engine, get_db
+
+model.Base.metadata.create_all(bind=engine)
 
 # Initialize the FastAPI
 app = FastAPI()
@@ -7,3 +12,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the TaskPulse API! The brain is online."}
+
+@app.get("/test-db")
+def test_db(db: Session = Depends(get_db)):
+    return {"message": "Database session successfully created!"}
